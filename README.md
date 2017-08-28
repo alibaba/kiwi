c7ef40da95ea829d4cf4c36ee9f01531738e6654
linhuiw <solesnip@gmail.com>
2017-08-28 21:30:34 +0800

    Change default export name & update readme

--- README.md
+++ README.md
@@ -5,10 +5,13 @@ I18N tools for universal javascrit apps, easy use & better api;
 
 Init this I18N tools
 ```javascript
-I18N.init('en-uk', {
+IntlFormat.init('en-uk', {
   'en-uk': {
     test: 'testvalue',
     testTemplate: 'you have ${value} unread message'
+  },
+  'zh-cn': {
+    lang: '语言'
   }
 });
 ```
@@ -17,13 +20,18 @@ Then use it in your components
 ```javascript
 import IntlFormat from 'intl-format';
 
-IntlFormat.test // testvalue;
+IntlFormat.test; // testvalue;
 
-IntlFormat.get('test')   // testvalue;
+IntlFormat.get('test'); // testvalue;
 
-I18N.template(I18N.testTemplate, {
+IntlFormat.template(IntlFormat.testTemplate, {
   value: three
 }); // value is 'you have three unread message'
+```
+
+How to change default language?
+```javascript
+IntlFormat.setLang('zh-cn'); // change the default language to zh-cn;
 ```
 
 ### License
--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,9 @@
 import * as assert from 'assert';
-import I18N from '../src/index';
+import IntlFormat from '../src/index';
 
 describe('intl-format', function() {
   before(function() {
-    I18N.init('zh-cn', {
+    IntlFormat.init('zh-cn', {
       'zh-cn': {
         test: '测试',
         testTemplate: '你有${value}条未读通知'
@@ -12,14 +12,14 @@ describe('intl-format', function() {
   });
   describe('get current value from certain language', function() {
     it('get key test value for current lanuage', function() {
-      assert.equal(I18N.test, '测试');
+      assert.equal(IntlFormat.test, '测试');
     });
     it('Get method: get key test value for current lanuage', function() {
-      assert.equal(I18N.get('test'), '测试');
+      assert.equal(IntlFormat.get('test'), '测试');
     });
     it('Template method: get template values for current lanuage', function() {
       assert.equal(
-        I18N.template(I18N.testTemplate, {
+        IntlFormat.template(IntlFormat.testTemplate, {
           value: 3
         }),
         '你有3条未读通知'
