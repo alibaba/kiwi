1dbfa37cce58bd08b50dbcc91d436a633e61cf5e
linhuiw <solesnip@gmail.com>
2017-08-29 19:55:39 +0800

    update version

--- built/src/index.js
+++ built/src/index.js
@@ -9,20 +9,18 @@ var __assign = (this && this.__assign) || Object.assign || function(t) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("proxy-polyfill");
-var I18N = {
-    __lang__: 'zh-cn',
-    __data__: {},
-    __metas__: {},
-    setLang: function (lang) {
-        I18N.__lang__ = lang;
-        I18N.__data__ = I18N.__metas__[lang];
-    },
-    init: function (lang, metas) {
-        I18N.__lang__ = lang;
-        I18N.__metas__ = metas;
-        I18N.__data__ = metas[lang];
-    },
-    getProp: function (obj, is, value) {
+var lodash_1 = require("lodash");
+var I18N = (function () {
+    function I18N(lang, metas) {
+        this.__lang__ = lang;
+        this.__metas__ = metas;
+        this.__data__ = metas[lang];
+    }
+    I18N.prototype.setLang = function (lang) {
+        this.__lang__ = lang;
+        this.__data__ = this.__metas__[lang];
+    };
+    I18N.prototype.getProp = function (obj, is, value) {
         if (typeof is === 'string') {
             is = is.split('.');
         }
@@ -37,32 +35,41 @@ var I18N = {
             if (value !== undefined && obj[prop] === undefined) {
                 obj[prop] = {};
             }
-            return I18N.getProp(obj[prop], is, value);
+            return this.getProp(obj[prop], is, value);
         }
-    },
-    template: function (str, args) {
-        if (!str)
+    };
+    I18N.prototype.template = function (str, args) {
+        var _this = this;
+        if (!str) {
             return '';
+        }
         return str.replace(/\$\{(.+?)\}/g, function (match, p1) {
-            return I18N.getProp(__assign({}, args, I18N.__data__), p1);
+            return _this.getProp(__assign({}, args, _this.__data__), p1);
         });
-    },
-    get: function (str) {
-        return Reflect.get(I18N.__data__, str);
+    };
+    I18N.prototype.get = function (str) {
+        return lodash_1.get(this.__data__, str);
+    };
+    return I18N;
+}());
+var IntlFormat = {
+    init: function (lang, metas) {
+        var i18n = new I18N(lang, metas);
+        var getLang = new Proxy(i18n, {
+            get: function (target, property, receiver) {
+                if (property === 'setLang' ||
+                    property === 'get' ||
+                    property === 'template' ||
+                    property === 'init') {
+                    return function () {
+                        return target[property].apply(i18n, arguments);
+                    };
+                }
+                return i18n.get(property);
+            }
+        });
+        return getLang;
     }
 };
-var getLang = new Proxy(I18N, {
-    get: function (target, property, receiver) {
-        if (property === 'setLang' ||
-            property === 'get' ||
-            property === 'template' ||
-            property === 'init') {
-            return function () {
-                return target[property].apply(this, arguments);
-            };
-        }
-        return I18N.__data__[property];
-    }
-});
-exports.getLang = getLang;
-exports.default = getLang;
+exports.IntlFormat = IntlFormat;
+exports.default = IntlFormat;
--- built/test/test.js
+++ built/test/test.js
@@ -2,26 +2,48 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var assert = require("assert");
 var index_1 = require("../src/index");
-describe('intl-format', function () {
+describe('IntlFormat', function () {
+    var intlFormat;
     before(function () {
-        index_1.default.init('zh-cn', {
+        intlFormat = index_1.default.init('zh-cn', {
             'zh-cn': {
                 test: '测试',
-                testTemplate: '你有${value}条未读通知'
+                testTemplate: '你有${value}条未读通知',
+                foo: {
+                    bar: 'foobar'
+                }
             }
         });
     });
     describe('get current value from certain language', function () {
         it('get key test value for current lanuage', function () {
-            assert.equal(index_1.default.test, '测试');
+            assert.equal(intlFormat.test, '测试');
         });
         it('Get method: get key test value for current lanuage', function () {
-            assert.equal(index_1.default.get('test'), '测试');
+            assert.equal(intlFormat.get('test'), '测试');
         });
         it('Template method: get template values for current lanuage', function () {
-            assert.equal(index_1.default.template(index_1.default.testTemplate, {
+            assert.equal(intlFormat.template(intlFormat.testTemplate, {
                 value: 3
             }), '你有3条未读通知');
+        });
+        it('Different instance values', function () {
+            var intlFormat1 = index_1.default.init('zh-cn', {
+                'zh-cn': {
+                    test: 'firstvalue'
+                }
+            });
+            var intlFormat2 = index_1.default.init('zh-cn', {
+                'zh-cn': {
+                    test: 'secondvalue'
+                }
+            });
+            assert.equal(intlFormat1.test, 'firstvalue');
+            assert.equal(intlFormat2.test, 'secondvalue');
+        });
+        it('Get deep value', function () {
+            assert.equal(intlFormat.foo.bar, 'foobar');
+            assert.equal(intlFormat.get('foo.bar'), 'foobar');
         });
     });
 });
