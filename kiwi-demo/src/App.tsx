95fccce878dd2afba872449712c59bb9f57eebee
linhuiw <solesnip@gmail.com>
2019-02-01 11:06:28 +0800

    feat: add kiwi demo dir

---
+++ kiwi-demo/src/App.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import './App.css';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          Hello kiwi 🐤
+          <br/>
+          This is a kiwi demo
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
