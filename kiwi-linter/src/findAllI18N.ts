50fa86edd839302f25e0ad7b145b660d8d51a86f
linhuiw <solesnip@gmail.com>
2019-02-18 17:05:26 +0800

    fix: fix find all i18n

--- kiwi-linter/src/findAllI18N.ts
+++ kiwi-linter/src/findAllI18N.ts
@@ -13,21 +13,25 @@ import { findInHtmls } from './findInHtmls';
 import { findI18NPositions } from './findI18NPositions';
 import { transformPosition } from './lineAnnotation';
 
-function extractObject(obj, keys = []as string[]): Array<Item> {
+function extractObject(obj, keys = [] as string[]): Array<Item> {
   if (typeof obj === 'string') {
-    return [{
-      keys,
-      value: obj,
-    }];
+    return [
+      {
+        keys,
+        value: obj
+      }
+    ];
   }
   const objKeys = Object.keys(obj);
 
-  return objKeys.map(key => {
-    const value = obj[key];
-    const currKeys = [...keys, key];
+  return objKeys
+    .map(key => {
+      const value = obj[key];
+      const currKeys = [...keys, key];
 
-    return extractObject(value, currKeys);
-  }).reduce((pre, next) => pre.concat(...next), []);
+      return extractObject(value, currKeys);
+    })
+    .reduce((pre, next) => pre.concat(...next), []);
 }
 
 /**
@@ -39,48 +43,68 @@ export function findAllI18N() {
 
   const getDesc = (item: Item) => item.value + ' I18N.' + item.keys.join('.');
 
-  vscode.window
-    .showQuickPick(allItems.map(getDesc))
-    .then(async selected => {
-      const foundItem = allItems.find(item => getDesc(item) === selected);
+  vscode.window.showQuickPick(allItems.map(getDesc)).then(async selected => {
+    const foundItem = allItems.find(item => getDesc(item) === selected);
 
-      if (!foundItem) {
-        return;
-      }
+    if (!foundItem) {
+      return;
+    }
 
-      const [target, ...restKeys] = foundItem.keys;
-      const uri = vscode.Uri.file(path.join(LANG_PREFIX, target + '.ts'));
-      const content = (await fs.readFile(path.join(LANG_PREFIX, target + '.ts'))).toString('utf8');
+    const [target, ...restKeys] = foundItem.keys;
+    const uri = vscode.Uri.file(path.join(LANG_PREFIX, target + '.ts'));
+    const content = (await fs.readFile(
+      path.join(LANG_PREFIX, target + '.ts')
+    )).toString('utf8');
 
-      const pos = findPositionInCode('"' + restKeys[restKeys.length - 1] + '"', content);
-      await vscode.workspace.openTextDocument(uri);
+    const preCodeContent = '"' + restKeys[restKeys.length - 1] + '"';
+    const newCodeContent = ' ' + restKeys[restKeys.length - 1] + ':';
+    let pos = findPositionInCode(preCodeContent, content);
 
-      try {
-        const tsLocations = await vscode.commands.executeCommand<vscode.Location[]>('vscode.executeReferenceProvider', uri, pos);
-        const templateLocations = await findInHtmls('I18N.' + foundItem.keys.join('.'));
-        const locations = [...tsLocations, ...templateLocations];
-        const [self, ...reals] = locations;
-        if (reals.length === 1) {
-          const uri = reals[0].uri;
-          const range = reals[0].range;
+    if (!pos) {
+      pos = findPositionInCode(newCodeContent, content);
+    }
 
-          const doc = await vscode.workspace.openTextDocument(uri);
-          await vscode.window.showTextDocument(doc);
+    await vscode.workspace.openTextDocument(uri);
 
-          vscode.window.activeTextEditor.selection = new vscode.Selection(
-            range.start,
-            range.end
-          );
-          vscode.window.activeTextEditor.revealRange(
-            range,
-            vscode.TextEditorRevealType.InCenter
-          );
-        } else if (reals.length > 1) {
-          vscode.window.showQuickPick(reals.map(real => path.relative(vscode.workspace.rootPath, real.uri.fsPath))).then(async item => {
-            const index = reals.findIndex(real => real.uri.fsPath.includes(item));
+    try {
+      const tsLocations = await vscode.commands.executeCommand<
+        vscode.Location[]
+      >('vscode.executeReferenceProvider', uri, pos);
 
-            const uri = reals[index].uri;
-            const range = reals[index].range;
+      const templateLocations = await findInHtmls(
+        'I18N.' + foundItem.keys.join('.')
+      );
+      const locations = [...tsLocations.slice(1), ...templateLocations];
+
+      if (locations.length === 1) {
+        const uri = locations[0].uri;
+        const range = locations[0].range;
+
+        const doc = await vscode.workspace.openTextDocument(uri);
+        await vscode.window.showTextDocument(doc);
+
+        vscode.window.activeTextEditor.selection = new vscode.Selection(
+          range.start,
+          range.end
+        );
+        vscode.window.activeTextEditor.revealRange(
+          range,
+          vscode.TextEditorRevealType.InCenter
+        );
+      } else if (locations.length > 1) {
+        vscode.window
+          .showQuickPick(
+            locations.map(real =>
+              path.relative(vscode.workspace.rootPath, real.uri.fsPath)
+            )
+          )
+          .then(async item => {
+            const index = locations.findIndex(real =>
+              real.uri.fsPath.includes(item)
+            );
+
+            const uri = locations[index].uri;
+            const range = locations[index].range;
             const doc = await vscode.workspace.openTextDocument(uri);
             await vscode.window.showTextDocument(doc);
 
@@ -93,13 +117,13 @@ export function findAllI18N() {
               vscode.TextEditorRevealType.InCenter
             );
           });
-        } else {
-          vscode.window.showInformationMessage('未被使用！');
-        }
-      } catch (e) {
-        debugger;
+      } else {
+        vscode.window.showInformationMessage('未被使用！');
       }
-    });
+    } catch (e) {
+      debugger;
+    }
+  });
 }
 
 /**
--- kiwi-linter/src/utils.ts
+++ kiwi-linter/src/utils.ts
@@ -11,19 +11,16 @@ import * as vscode from 'vscode';
  * @param prefix
  */
 export function flatten(obj, prefix?) {
-  var propName = (prefix) ? prefix + '.' : '',
+  var propName = prefix ? prefix + '.' : '',
     ret = {};
 
   for (var attr in obj) {
-
     if (_.isArray(obj[attr])) {
       var len = obj[attr].length;
       ret[attr] = obj[attr].join(',');
-    }
-    else if (typeof obj[attr] === 'object') {
+    } else if (typeof obj[attr] === 'object') {
       _.extend(ret, flatten(obj[attr], propName + attr));
-    }
-    else {
+    } else {
       ret[propName + attr] = obj[attr];
     }
   }
@@ -33,9 +30,14 @@ export function flatten(obj, prefix?) {
 /**
  * 查找当前位置的 Code
  */
-export function findPositionInCode(text: string, code:string) {
+export function findPositionInCode(text: string, code: string) {
   const lines = code.split('\n');
   const lineNum = lines.findIndex(line => line.includes(text));
+
+  if (lineNum === -1) {
+    return null;
+  }
+
   const chNum = lines[lineNum].indexOf(text);
 
   return new vscode.Position(lineNum, chNum);
