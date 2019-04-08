492b32584f5b50546a9d3aa4a0f799da8a1b551a
linhuiw <solesnip@gmail.com>
2019-04-08 15:18:07 +0800

    feat: compatible zh_CN lang tag

--- kiwi-linter/src/const.ts
+++ kiwi-linter/src/const.ts
@@ -10,17 +10,28 @@ import { getAllFiles } from './utils';
  * 适配不同的语言文件夹位置
  */
 function dirAdaptor() {
-  const firstLangPerfix = `${vscode.workspace.rootPath}/.kiwi/zh-CN/`;
-  const backupLangPrefix = `${vscode.workspace.rootPath}/langs/zh-CN/`;
+  const kiwiLangPerfix = `${vscode.workspace.rootPath}/.kiwi/zh-CN/`;
+  const langPrefix = `${vscode.workspace.rootPath}/langs/zh-CN/`;
 
-  if (fs.existsSync(firstLangPerfix)) {
-    return firstLangPerfix;
-  } else if (fs.existsSync(backupLangPrefix)) {
-    return backupLangPrefix;
+  /** 兼容 zh_CN 情况 */
+  const _kiwiLangPerfix = `${vscode.workspace.rootPath}/.kiwi/zh_CN/`;
+  const _langPrefix = `${vscode.workspace.rootPath}/langs/zh_CN/`;
+
+  if (fs.existsSync(kiwiLangPerfix)) {
+    return kiwiLangPerfix;
+  } else if (fs.existsSync(langPrefix)) {
+    return langPrefix;
+  } else if (fs.existsSync(_kiwiLangPerfix)) {
+    return _kiwiLangPerfix;
+  } else if (fs.existsSync(_langPrefix)) {
+    return _langPrefix;
   } else {
     const files = getAllFiles(`${vscode.workspace.rootPath}/`);
     const matchFiles = files.filter((fileName) => {
-      if (fileName.includes('/.kiwi/zh-CN/index.ts') || fileName.includes('/langs/zh-CN/index.ts')) {
+      if (fileName.includes('/.kiwi/zh-CN/index.ts')
+        || fileName.includes('/langs/zh-CN/index.ts')
+        || fileName.includes('/.kiwi/zh_CN/index.ts')
+        || fileName.includes('/langs/zh_CN/index.ts')) {
         return true;
       }
       return false;
