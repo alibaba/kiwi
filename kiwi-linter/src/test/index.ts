8a176b4e15e6060108152514d24ce320ca5c3e16
xiaohuoni <448627663@qq.com>
2019-04-12 14:22:36 +0800

    test prettier

--- kiwi-linter/src/test/index.ts
+++ kiwi-linter/src/test/index.ts
@@ -16,7 +16,7 @@ import * as testRunner from 'vscode/lib/testrunner';
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
 testRunner.configure({
    ui: 'tdd', // the TDD UI is being used in extension.test.ts (suite, test, etc.)
-  useColors: true // colored output from test results
+   useColors: true // colored output from test results
 });
 
 module.exports = testRunner;
